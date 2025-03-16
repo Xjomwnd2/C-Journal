437c3d34ff3c2123b63b7550cb054449cc48fb3c
Users <jmwaura@byupathway.edu>
2025-03-16 11:50:12 +0300

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,14 @@
-static void DisplayMenu()
+using System;
+
+class Program
 {
+    static void Main(string[] args)
+    {
     Console.WriteLine("\nPlease select one of the following choices:");
-    Console.WriteLine("1. Write a new entry (or type 'write new entry')");
-    Console.WriteLine("2. Display the journal (or type 'display journal')");
-    Console.WriteLine("3. Save the journal to a file (or type 'save journal')");
-    Console.WriteLine("4. Load the journal from a file (or type 'load journal')");
-    Console.WriteLine("5. Quit (or type 'quit')");
+    Console.WriteLine("1. Write a new entry");
+    Console.WriteLine("2. Display the journal");
+    Console.WriteLine("3. Save the journal to a file");
+    Console.WriteLine("4. Load the journal from a file");
+    Console.WriteLine("5. Quit");
+    }
 }
