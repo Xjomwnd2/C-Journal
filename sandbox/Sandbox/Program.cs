a61810276a47b4d753d44f6c286a59558ce88db6
Users <jmwaura@byupathway.edu>
2025-03-16 12:03:43 +0300

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -28,3 +28,44 @@ public class Journal
         Console.WriteLine("Journal saved to file.");
     }
 }
+
+public class Program
+{
+    public static void Main()
+    {
+        Journal journal = new Journal();
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("\nPlease select one of the following choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Quit");
+            Console.Write("What would you like to do? ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    journal.WriteNewEntry();
+                    break;
+                case "2":
+                    journal.DisplayAllEntries();
+                    break;
+                case "3":
+                    journal.SaveToFile();
+                    break;
+                case "4":
+                    exit = true;
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
