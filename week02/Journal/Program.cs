10372b6dae4476629e610574281371f1557c6bb1
Users <jmwaura@byupathway.edu>
2025-03-16 10:30:27 +0300

    COMMIT

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
+     Journal journal = new Journal();
+        bool running = true;
+        
+        Console.WriteLine("Welcome to the Journal Program!");
+        
+        while (running)
+        {
+            DisplayMenu();
+            Console.Write("What would you like to do? ");
+            string choice = Console.ReadLine();
+            
+            switch (choice)
+            {
+                case "1":
+                    journal.WriteNewEntry();
+                    break;
+                case "2":
+                    journal.DisplayAllEntries();
+                    break;
+                case "3":
+                    journal.SaveToFile();
+                    break;
+                case "4":
+                    journal.LoadFromFile();
+                    break;
+                case "5":
+                    running = false;
+                    Console.WriteLine("Thank you for using the Journal Program. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+            
+            Console.WriteLine();
+        }
+    }
+    
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display the journal");
+        Console.WriteLine("3. Save the journal to a file");
+        Console.WriteLine("4. Load the journal from a file");
+        Console.WriteLine("5. Quit");
+    }
+
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
