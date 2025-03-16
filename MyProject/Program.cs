74916dfdfaa91a9e6efd39b1ef4b5ce0860a3f48
Users <jmwaura@byupathway.edu>
2025-03-16 11:03:43 +0300

    commit

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("\n===== JOURNAL MENU =====");
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display the journal");
+        Console.WriteLine("3. Save the journal to a file");
+        Console.WriteLine("4. Load the journal from a file");
+        Console.WriteLine("5. Quit");
+        Console.Write("Enter your choice: ");
+
+        return Console.ReadLine().Trim();
